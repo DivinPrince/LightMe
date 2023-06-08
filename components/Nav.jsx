40d4742cf50c031c4ef918b3e5d598b685d5414f
irasubiza1 <dpcode36@gmail.com>
2023-06-08 16:25:56 +0200

    initial

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -58,7 +58,7 @@ export default function Nav() {
                         <button
                             type='button'
                             onClick={() => setToogleDropIn((prev) => !prev)}
-                            className='outline_btn btn'
+                            className='glassmorphism'
                         >
                             Sign In
                         </button>
