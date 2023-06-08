b59bc3e785f3874c4638de48645c62b070cc36ad
irasubiza1 <dpcode36@gmail.com>
2023-06-08 22:11:15 +0200

    init

---
+++ app/api/project/route.js
@@ -0,0 +1,14 @@
+import { connectToDB } from "@utils/database";
+import Project from "@models/project";
+export const GET = async (req) =>{
+    try {
+        await connectToDB()
+        const projects = await Project.find({}).populate('creator')
+
+        return new Response(JSON.stringify(projects),{status: 200})
+
+    } catch (error) {
+        console.log(error.message);
+        return new Response('failed',{status: 500})
+}
+}
---
+++ app/projects/page.jsx
@@ -0,0 +1,45 @@
+'use client'
+import ProjectCard from '@components/ProjectCard'
+import React,{useEffect,useState} from 'react'
+const ProjectLists = ({data,handleLink}) =>{
+    return (
+        <div className='promp_layout'>
+            {data.map(project =>(
+                <ProjectCard
+                    key={project.id}
+                    project={project}
+                />
+            ))
+
+            }
+        </div>
+    )
+}
+
+
+export default function page() {
+    const [projects, setProjects] = useState([])
+    useEffect(()=>{
+        const fetchProjects = async () =>{
+            const res = await fetch('/api/project')
+            const data = await res.json()
+
+            setProjects(data)
+        }
+
+        fetchProjects()
+    },[])
+  return (
+    <section>
+        page
+        <ProjectLists
+            data={projects} 
+            handleLink={()=>{}}
+        />
+    </section>
+
+  )
+  
+
+}
+
