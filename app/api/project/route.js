2dff870e0847b587b365458a9503494b320dff16
irasubiza1 <dpcode36@gmail.com>
2023-06-12 19:03:03 +0200

    init

--- app/api/project/route.js
+++ app/api/project/route.js
@@ -1,14 +1,14 @@
-// import { connectToDB } from "@utils/database";
-// import Project from "@models/project";
-// export const GET = async (req) =>{
-//     try {
-//         await connectToDB()
-//         const projects = await Project.find({}).populate('creator')
+import { connectToDB } from "@utils/database";
+import Project from "@models/project";
+export const GET = async (req) =>{
+    try {
+        await connectToDB()
+        const projects = await Project.find({}).populate('creator')
 
-//         return new Response(JSON.stringify(projects),{status: 200})
+        return new Response(JSON.stringify(projects),{status: 200})
 
-//     } catch (error) {
-//         console.log(error.message);
-//         return new Response('failed',{status: 500})
-// }
-// }
+    } catch (error) {
+        console.log(error.message);
+        return new Response('failed',{status: 500})
+}
+}
