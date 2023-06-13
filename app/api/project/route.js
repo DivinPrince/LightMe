353ebe62c6baa7a01b35b9b8e9d7c223b46c0a81
irasubiza1 <dpcode36@gmail.com>
2023-06-13 18:03:32 +0200

    init

--- app/api/project/route.js
+++ app/api/project/route.js
@@ -1,14 +1,18 @@
 import { connectToDB } from "@utils/database";
 import Project from "@models/project";
-export const GET = async (req) =>{
-    try {
-        await connectToDB()
-        const projects = await Project.find({}).populate('creator')
 
-        return new Response(JSON.stringify(projects),{status: 200})
+export const GET = async (req) => {
+  try {
+    await connectToDB();
+    const projects = await Project.find({}).populate("creator");
 
-    } catch (error) {
-        console.log(error.message);
-        return new Response('failed',{status: 500})
-}
-}
+    // Perform an update operation on the projects
+    // Example: projects[0].title = "Updated Title";
+    // Save the updated project: await projects[0].save();
+
+    return new Response(JSON.stringify(projects), { status: 200 });
+  } catch (error) {
+    console.log(error.message);
+    return new Response("failed", { status: 500 });
+  }
+};
